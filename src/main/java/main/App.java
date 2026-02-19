5abc478933040a0958e11f6f6ce641a4cf564af2
Kati Liukkonen <kati.liukkonen@student.lut.fi>
2026-02-19 17:25:12 +0200

    Lisätty valikko pääohjelmaan

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -6,13 +6,50 @@
 
 package main;
 
-
-public final class App {
-    private App() {
-    }
+import java.util.Scanner;
 
 
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner sc = new Scanner(System.in);
+    
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("1) Lisää kirja");
+            System.out.println("2) Listaa kirjat");
+            System.out.println("3) Lainaa fiktiokirja");
+            System.out.println("4) Palauta fiktiokirja");
+            System.out.println("0) Lopeta ohjelma");
+            
+            if(sc.hasNext()) {
+                int i = 0;
+                String input = sc.next();
+                i = Integer.parseInt(input);
+            
+                switch (i) {
+                    case 1:
+                        // Lisää kirja
+                        break;
+                    case 2:
+                        // Listaa kirjat
+                        break;
+                    case 3:
+                        // Lainaa fiktiokirja
+                        break;
+                    case 4:
+                        // Palauta fiktiokirja
+                        break;
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Syöte oli väärä.");
+                        break;
+                }
+            }
+        }
+    sc.close();    
     }
 }
